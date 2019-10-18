343ff00e806663723ad0852bbaef037558c338a4
Dmitriy Ukhin <dukhin@teledes.com.ua>
2019-10-18 17:19:18 +0300

    save commit

---
+++ src/main/java/com/silentium_noxe/tests/offlineapp/OfflineAppApplication.java
@@ -0,0 +1,13 @@
+package com.silentium_noxe.tests.offlineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflineAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfflineAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silentium_noxe/tests/offlineapp/OfflineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silentium_noxe.tests.offlineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflineAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
